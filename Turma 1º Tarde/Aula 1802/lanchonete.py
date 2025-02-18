fb7c549aaa0c25d348a65c2045221a96397e589d
Tarik Ponciano <40394108+TarikPonciano@users.noreply.github.com>
2025-02-18 14:06:06 -0300

    Create lanchonete.py

---
+++ Turma 1º Tarde/Aula 1802/lanchonete.py
@@ -0,0 +1,18 @@
+print("BEM VINDO AO SISTEMA ALIMENTAR PARÁ LANCHES")
+
+print()
+
+print('''
+        Cardápio:
+
+        COD | Nome Lanche | Preço R$      
+
+        100 | Cachorro Quente | R$ 1,10
+        101 | Baurú Simples   | R$ 1,30
+        102 | Baurú C/Ovo     | R$ 1,50
+        103 | Hamburguer      | R$ 1,10
+        104 | Cheeseburguer   | R$ 1,30
+        105 | Refrigerante    | R$ 1,00
+''')
+
+codigo = input("Digite o codigo:")
