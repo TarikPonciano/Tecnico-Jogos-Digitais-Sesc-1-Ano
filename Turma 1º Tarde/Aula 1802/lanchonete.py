e881b2bbbe52387a75f8d2d3b31cffb59fa2cc34
Tarik Ponciano <40394108+TarikPonciano@users.noreply.github.com>
2025-02-18 14:26:21 -0300

    Update lanchonete.py

--- Turma 1º Tarde/Aula 1802/lanchonete.py
+++ Turma 1º Tarde/Aula 1802/lanchonete.py
@@ -23,6 +23,27 @@ if (codigo == "100"):
 elif (codigo == "101"):
     produto = "Baurú Simples"
     preco = 1.30
+elif (codigo == "102"):
+    produto = "Bauru C/Ovo"
+    preco = 1.50
+elif (codigo == "103"):
+    produto = "Hamburguer"
+    preco = 1.10
+elif (codigo == "104"):
+    produto = "Cheeseburguer"
+    preco = 1.30
+elif (codigo == "105"):
+    produto = "Refrigerante"
+    preco = 1.00
 else:
     produto = "Produto Inválido"
     preco = 0
+
+print(f"O produto escolhido foi {produto}!")
+
+if (produto == "Produto Inválido"):
+    print("Da próxima vez escolha um produto válido!")
+else:
+    quantidade = int(input("Digite a quantidade desejada: "))
+    total = preco * quantidade
+    print(f"Você comprou {quantidade} unidades de {produto} e pagou R$ {total:.2f}!")
