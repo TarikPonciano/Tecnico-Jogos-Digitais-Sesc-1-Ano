d07269f63cb9e8ff222e92baf16af9b9bdea25fe
Tarik Ponciano <40394108+TarikPonciano@users.noreply.github.com>
2025-02-19 16:59:40 -0300

    Update palpite.py

--- Turma 1º Tarde/Aula 1902/palpite.py
+++ Turma 1º Tarde/Aula 1902/palpite.py
@@ -5,3 +5,29 @@
 #Utilizando o loop for, permita que a pessoa tenha 3 tentativas
 
 #Se a pessoa acertar, termine o loop mais cedo    
+import random 
+
+alunoSorteado = random.randint(1,36)
+numeroSecreto = random.randint(1,100)
+
+print("Aluno Sorteado:", alunoSorteado)
+tentativas = 0
+for i in range(3):
+    palpite = int(input("Digite um número de 1 a 100: "))
+    tentativas += 1
+    if (palpite == numeroSecreto):
+        print("Você acertou!")
+        break
+    else:
+        print("Você errou!")
+        if (tentativas == 3):
+            print("Fim das suas tentativas!")
+            break
+        if (palpite > numeroSecreto):
+            print("Dê um chute menor!")
+        else:
+            print("Dê um chute maior!")
+
+print("FIM DE JOGO")
+
+print("O número era", numeroSecreto)
