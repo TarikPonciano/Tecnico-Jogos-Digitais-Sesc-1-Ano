0336ab30f61bcab11784c34b64e7575bc20ba413
Tarik Ponciano <40394108+TarikPonciano@users.noreply.github.com>
2025-02-19 09:44:21 -0300

    Create atividades.py

---
+++ Turma 1º Manhã/Aula 1902/atividades.py
@@ -0,0 +1,5 @@
+# Crie um programa que, usando loop for, pede 4 notas de um aluno. Calcule a soma dessas notas, a média dessas notas e exiba a média final e a situação do aluno seguindo as regras abaixo:
+#Se o aluno tirou um nota maior ou igual a 7 e menor ou igual a 10, ele está aprovado.
+#Se não, se o aluno tirou uma nota maior ou igual a 4 e menor que 7, o aluno está de recuperação.
+#Se não, se o aluno tirou uma nota maior ou igual a 0 e menor que 4, o aluno está reprovado.
+#Caso a média seja diferente de todas essas opcoes, a média é inválida
