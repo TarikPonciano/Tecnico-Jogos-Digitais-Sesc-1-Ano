1c3063107366b35fe6e76f31d30b661d7052b96d
Tarik Ponciano <40394108+TarikPonciano@users.noreply.github.com>
2025-02-19 10:59:16 -0300

    Atualizando

--- Turma 1º Manhã/Aula 1902/notas.py
+++ Turma 1º Manhã/Aula 1902/notas.py
@@ -3,3 +3,21 @@
 #Se não, se o aluno tirou uma nota maior ou igual a 4 e menor que 7, o aluno está de recuperação.
 #Se não, se o aluno tirou uma nota maior ou igual a 0 e menor que 4, o aluno está reprovado.
 #Caso a média seja diferente de todas essas opcoes, a média é inválida
+soma = 0
+
+for i in range(4):
+    nota = float(input("Digite uma nota:"))
+    soma += nota
+
+media = soma / 4
+
+print(f"Sua média final foi: {media}")
+
+if (media >= 7 and media <= 10):
+    print("Você está aprovado!")
+elif (media >= 4 and media < 7):
+    print("Você está de recuperação!")
+elif (media >= 0 and media < 4):
+    print("Você está reprovado!")
+else:
+    print("Média inválida!")
---
+++ Turma 1º Manhã/Aula 1902/palpite.py
@@ -0,0 +1,7 @@
+#Crie um programa que possui um número secreto definido por você. Permita que o usuário dê um palpite, se o palpite for correto, ou seja, palpite == numeroSecreto, você deverá imprimir 'Vitória'. Se o palpite for incorreto você deverá imprimir 'Derrota'.
+
+#Ao errar indique se o número é maior ou menor que o palpite.
+
+#Utilizando o loop for, permita que a pessoa tenha 3 tentativas
+
+#Se a pessoa errar, termine o loop mais cedo
