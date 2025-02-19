3bf2f92678472a04c90158123942c040ec6b5453
Tarik Ponciano <40394108+TarikPonciano@users.noreply.github.com>
2025-02-19 14:01:43 -0300

    Create testes.py

---
+++ Turma 1º Tarde/Aula 1902/testes.py
@@ -0,0 +1,19 @@
+# Crie um programa que pede seu nome e imprime "Olá {nome}!" 5 vezes.
+
+nome = input("Digite seu nome:")
+
+for i in range(5):
+    print(f"Olá, {nome}!")
+
+# Crie um programa que repete 5 vezes as seguintes instruções:
+# Pede o nome do usuário
+# Imprime "Olá, {nome}!"
+
+# Crie um programa que use loop for para calcular a soma de 1 até 1000
+
+# Crie um programa que use loop for para calcular a média da soma de 1 até 1000
+
+# Crie um programa que exibe apenas os números pares de 1 até 1000
+
+for i in range(1,6):
+    print(f"{i}. Bom dia")
