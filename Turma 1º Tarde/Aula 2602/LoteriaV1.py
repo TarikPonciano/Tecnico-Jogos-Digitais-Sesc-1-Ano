75057517626d6374cd1948a3ea5ec980d08cce18
Tarik Ponciano <40394108+TarikPonciano@users.noreply.github.com>
2025-02-26 14:45:13 -0300

    Create LoteriaV1.py

---
+++ Turma 1º Tarde/Aula 2602/LoteriaV1.py
@@ -0,0 +1,14 @@
+bilhete = ""
+
+for i in range(6):
+
+    numeroDaSorte = input(f"Escreva um numero de 1 a 60:")
+
+    bilhete += " - " + numeroDaSorte
+
+
+print(f"Bilhete da Sorte: {bilhete}")
+
+
+
+
