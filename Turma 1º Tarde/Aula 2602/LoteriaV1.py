11a909f1ef61c1118538f4dfdc73897d7e511a61
Tarik Ponciano <40394108+TarikPonciano@users.noreply.github.com>
2025-02-26 14:58:01 -0300

    Update LoteriaV1.py

--- Turma 1º Tarde/Aula 2602/LoteriaV1.py
+++ Turma 1º Tarde/Aula 2602/LoteriaV1.py
@@ -1,8 +1,10 @@
 bilhete = ""
 
+numeroPremiado = "6"
+
 for i in range(6):
 
-    numeroDaSorte = input(f"Escreva um numero de 1 a 60:")
+    numeroDaSorte = input(f"Escreva um numero de 01 a 60:")
 
     if (i == 0):
         bilhete += numeroDaSorte
@@ -11,6 +13,10 @@ for i in range(6):
 
 print(f"Bilhete da Sorte: {bilhete}")
 
+if (numeroPremiado in bilhete):
+    print("EBAAAAAAA. Parabéns você venceu! :)")
+else:
+    print("AAaaaaaahhhh. Que pena, você perdeu! :(")
 
 
 
