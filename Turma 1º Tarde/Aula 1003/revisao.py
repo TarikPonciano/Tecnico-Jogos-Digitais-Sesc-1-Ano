a361245636783ae6da32daac0f48f268cdb40b28
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2025-03-10 14:40:46 -0300

    Update revisao.py

--- Turma 1º Tarde/Aula 1003/revisao.py
+++ Turma 1º Tarde/Aula 1003/revisao.py
@@ -8,14 +8,22 @@
 
 
 nome = input("Digite seu nome: ")
+
 idade = int(input("Digite sua idade: "))
+
 
 print(f"Olá meu nome é {nome} e tenho {idade} anos!")
 
 
 
 # Melhore a verificação de idade. Agora verifique se a pessoa é idosa (idade maior ou igual a 60), adulta (idade maior ou igual a 18 e menor que 60) ou jovem (idade menor que 18)
-if idade >= 18:
-    print("Você é maior de idade!")
+if idade >= 60:
+    print("Você é idose!")
+elif idade >= 18 and idade < 60:
+    print("Você é adulte!")
+elif idade < 18 and idade >= 0:
+    print("Você é jovem!")
 else:
-    print("Você é menor de idade!")
+    print("Você é um alienigene")
+
+# Pergunte para o usuário quantos animais de estimação ele tem. Na sequencia pergunte o nome de cada animal, utilizando repetição. Ao final, exiba o nome dos animais de estimação informados.
