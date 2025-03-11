54a376e7cbdb8720ca2b74aa431964ab7991f968
Tarik Ponciano <40394108+TarikPonciano@users.noreply.github.com>
2025-03-11 09:57:56 -0300

    Update exercicio1.py

--- Turma 1º Manhã/Aula 1103/exercicio1.py
+++ Turma 1º Manhã/Aula 1103/exercicio1.py
@@ -3,6 +3,7 @@ frutas = []
 # Peça que o usuário escreva o nome de 5 frutas e adicione-as à lista
 for i in range(5):
     fruta = input("Digite o nome de uma fruta: ")
+    
     frutas.append(fruta)
 
 # Após escrever o nome das frutas exiba o nome dela no terminal
@@ -10,7 +11,15 @@ print(frutas)
 
 contador = 1
 for fruta in frutas:
+    
     print(f"{contador}. {fruta}")
     contador += 1
     
 # Exiba na tela apenas as frutas que começam com a letra "A"
+
+print("Frutas que começam com a letra A")
+
+for fruta in frutas:
+    if (fruta.lower()[0] == "a"):
+        print(fruta)
+    
