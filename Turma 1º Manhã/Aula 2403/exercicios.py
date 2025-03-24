ab000ae1ae8fbaf9c6f065e7843f8907525f3cee
Tarik Ponciano <40394108+TarikPonciano@users.noreply.github.com>
2025-03-24 11:58:41 -0300

    Create exercicios.py

---
+++ Turma 1º Manhã/Aula 2403/exercicios.py
@@ -0,0 +1,14 @@
+# 1. Agenda de contatos
+# Crie um programa que simula um agenda de contatos
+# O programa deverá conter um dicionário que possui como chave o nome da pessoa e como valor o telefone daquela pessoa
+# Crie uma repetição e implemente as funcionalidades
+#   - Cadastrar novo contato
+#   - Ver o telefone de um contato específico
+#   - Remover um contato específico
+#   - Listar os contatos em ordem alfabética
+
+# 2. Loja de Equipamentos
+# Crie um programa que deverá conter um dicionário que possui como chave o nome de um equipamento e como valor o custo daquele equipamento
+# Exiba na tela os equipamentos e seus preços disponíveis na loja
+# Permita que o usuário escolha comprar um equipamento e subtraia seu valor do ouro total do usuário (criar variável ouro total)
+# Ao comprar um equipamento deve ser verificado que há ouro suficiente para a compra, e quando a compra ocorrer com sucesso deve-se remover o equipamento da loja e adicionar ao inventário do usuário (lista)
