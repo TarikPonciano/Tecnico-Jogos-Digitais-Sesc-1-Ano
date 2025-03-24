853c94478e0f2b2d3c5782f3b36bd4ad51c47a7f
Tarik Ponciano <40394108+TarikPonciano@users.noreply.github.com>
2025-03-24 09:53:36 -0300

    Update dicionarios.py

--- Turma 1º Manhã/Aula 2403/dicionarios.py
+++ Turma 1º Manhã/Aula 2403/dicionarios.py
@@ -22,4 +22,30 @@ Nacionalidade {pessoa["Nacionalidade"]}
 ''')
 
 
-#Crie um cadastro de pet, de um petshop, esse cadastro precisa possuir as informações Nome, Espécie, Peso e Idade. Use um dicionário para armazenar essas informações e imprima na tela a ficha desse paciente pet.
+#Crie um cadastro de pet, de um petshop, esse cadastro precisa possuir (usar input) as informações Nome, Espécie, Peso e Idade. Use um dicionário para armazenar essas informações e imprima na tela a ficha desse paciente pet.
+
+pet = {
+
+}
+
+pet["Nome"] = input("Digite o nome do pet: ")
+pet["Espécie"] = input("Digite a espécie do seu pet: ")
+pet["Peso"] = float(input("Digite o peso do seu pet:"))
+pet["Idade"] = int(input("Digite a idade do seu pet: "))
+pet["Tutor"] = input("Digite seu nome:")
+
+print(f'''
+Ficha do Paciente:
+      
+
+      Nome - {pet["Nome"]}
+      Espécie - {pet["Espécie"]}
+      Peso - {pet["Peso"]}
+      Idade - {pet["Idade"]}
+
+''')
+
+
+#Impressão automática de informações do dicionário
+# for key in pet.keys():
+#     print(f"{key} - {pet[key]}")
