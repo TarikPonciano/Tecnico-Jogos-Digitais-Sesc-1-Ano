bb8e22a77f6d300c4b154f69017f9c8d920ff696
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2025-04-10 16:57:34 -0300

    Update testeEscopo.py

--- Turma 1º Tarde/Aula 0904/testeEscopo.py
+++ Turma 1º Tarde/Aula 0904/testeEscopo.py
@@ -1,11 +1,27 @@
+def main():
+    print("Bem vindo ao meu programa")
+
+    print("No meu programa temos as funcionalidades: Elevar ao Quadrado, Inverter Numero")
+
+    numero = int(input("Digite um numero para usar no meu programa: "))
+
+    print("Numero ao quadrado: ", e(numero))
+    print("Numero invertido: ", reverso(numero))
+
 def e(b):
-    global a
     a = b * b
-    global c
-    c = 20
-    print(c)
     
     return a
 
-def reverso(num):
-    
+def reverso (num):
+    num = str(num)
+    return num[::-1]
+
+def f3(a):
+    global x
+    x = x + 1
+    print(a+x)
+
+x = 4
+f3(3)
+print(x)
