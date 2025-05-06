0e5e2bab82dae1487deede807610b8d95c57afa3
Tarik Ponciano <40394108+TarikPonciano@users.noreply.github.com>
2025-05-06 10:58:49 -0300

    Calculadora

--- Turma 1º Manhã/Projetos Godot/revisao-manha-0505/calculadora_matemática.gd
+++ Turma 1º Manhã/Projetos Godot/revisao-manha-0505/calculadora_matemática.gd
@@ -26,4 +26,32 @@ func _ready() -> void:
 			print("DIVISÃO POR 0 NÃO AUTORIZADA!")
 		else:
 			print("Resultado da Divisão: ", (float(numero1)/float(numero2)))
+	else:
+		print("OPERAÇÃO INVÁLIDA")
+
+
+
+func _on_botao_calcular_pressed() -> void:
+	#Verificar se os números foram preenchidos. Se os números não forem preenchidos imprimir no resultado um erro "PREENCHER NÚMEROS"
+	var numero1 = float($CampoNum1.text)
+	var numero2 = float($CampoNum2.text)
 	
+	var ops = $ListaOp.get_selected_items()
+	
+	if ops.size() == 0:
+		$RotuloResultado.text = "Resultado: OPERAÇÃO NÃO SELECIONADA!"
+		return
+		
+	var op = ops[0]
+	
+	if (op == 0):
+		$RotuloResultado.text = str("Resultado: ", (numero1+numero2))
+	elif (op == 1):
+		$RotuloResultado.text = str("Resultado: ", (numero1-numero2))
+	elif (op == 2):
+		$RotuloResultado.text = str("Resultado: ", (numero1*numero2))
+	elif (op == 3):
+		if (numero2 == 0):
+			$RotuloResultado.text = str("Resultado: DIVISÃO POR 0!")
+		else:
+			$RotuloResultado.text = str("Resultado: ", (numero1/numero2))
