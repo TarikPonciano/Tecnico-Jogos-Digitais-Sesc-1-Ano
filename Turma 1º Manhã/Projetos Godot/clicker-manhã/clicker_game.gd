33ac96af37fcb07b17194cfd4fa56b14f94c3a13
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2025-05-14 09:25:16 -0300

    Clicker Game

--- Turma 1º Manhã/Projetos Godot/clicker-manhã/clicker_game.gd
+++ Turma 1º Manhã/Projetos Godot/clicker-manhã/clicker_game.gd
@@ -3,14 +3,17 @@ extends Node2D
 var score = 0
 var power_click = 1
 var custo_upgrade_click = 10
+var qtd_upgrade_click = 0
 
 var power_passive = 0
 
 var qtd_ajudante1 = 0
 var custo_ajudante1 = 20
+var ganho_ajudante1 = 0.5
 
 var qtd_ajudante2 = 0
 var custo_ajudante2 = 100
+var ganho_ajudante2 = 1
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("ui_accept"):
@@ -21,7 +24,7 @@ func _process(delta: float) -> void:
 	
 func _on_botao_clicar_pressed() -> void:
 	score += power_click
-	$RotuloScore.text = str("Score: ", score," (+",power_passive,"/s)")
+	atualizar_interface()
 	
 
 
@@ -29,14 +32,13 @@ func _on_botao_upgrade_click_pressed() -> void:
 	
 	if score >= custo_upgrade_click:
 		
-		power_click += 1
 		score -= custo_upgrade_click
 		custo_upgrade_click *= 1.2
 		custo_upgrade_click = int(custo_upgrade_click)
+		qtd_upgrade_click += 1
 		
-		$RotuloScore.text = str("Score: ", score," (+",power_passive,"/s)")
-		$BotaoClicar.text = str("Clique Aqui! (+",power_click,")")
-		$BotaoUpgradeClick.text = str("Upgrade Click: ",custo_upgrade_click," points")
+		atualizar_ganhos()
+		atualizar_interface()
 
 
 func _on_comprar_ajudante_1_pressed() -> void:
@@ -46,17 +48,16 @@ func _on_comprar_ajudante_1_pressed() -> void:
 		custo_ajudante1 *= 1.3
 		custo_ajudante1 = int(custo_ajudante1)
 		qtd_ajudante1 += 1
-		power_passive += 0.5
 		
-		$RotuloScore.text = str("Score: ", score," (+",power_passive,"/s)")
-		$MenuAjudantes/Ajudante1/ComprarAjudante1.text = str("Comprar\n",custo_ajudante1," points")
-		$MenuAjudantes/Ajudante1/QuantidadeAjudante1.text = str("QTD\n",qtd_ajudante1)
+		atualizar_ganhos()
+		atualizar_interface()
 		
 
 
 func _on_timer_timeout() -> void:
 	score += power_passive
-	$RotuloScore.text = str("Score: ", score," (+",power_passive,"/s)")
+	atualizar_ganhos()
+	atualizar_interface()
 
 
 func _on_comprar_ajudante_2_pressed() -> void:
@@ -66,9 +67,23 @@ func _on_comprar_ajudante_2_pressed() -> void:
 		custo_ajudante2 *= 1.3
 		custo_ajudante2 = int(custo_ajudante2)
 		qtd_ajudante2 += 1
-		power_passive += 1.0
 		
-		$RotuloScore.text = str("Score: ", score," (+",power_passive,"/s)")
-		$MenuAjudantes/Ajudante2/ComprarAjudante2.text = str("Comprar\n",custo_ajudante2," points")
-		$MenuAjudantes/Ajudante2/QuantidadeAjudante2.text = str("QTD\n",qtd_ajudante2)
+		atualizar_ganhos()
+		atualizar_interface()
 		
+func atualizar_interface():
+	
+	$RotuloScore.text = str("Score: ",score,"(+",power_passive,")")
+	
+	$BotaoUpgradeClick.text = str("Upgrade Click: ",custo_upgrade_click)
+	
+	$MenuAjudantes/Ajudante1/QuantidadeAjudante1.text = str("QTD\n",qtd_ajudante1)
+	$MenuAjudantes/Ajudante1/ComprarAjudante1.text = str("Comprar \n", custo_ajudante1)
+	
+	$MenuAjudantes/Ajudante2/QuantidadeAjudante2.text = str("QTD\n",qtd_ajudante2)
+	$MenuAjudantes/Ajudante2/ComprarAjudante2.text = str("Comprar \n", custo_ajudante2)
+	
+func atualizar_ganhos():
+	power_passive = 0 + (qtd_ajudante1 * ganho_ajudante1) + (qtd_ajudante2 * ganho_ajudante2)
+	power_click = 1 + (1*qtd_upgrade_click)
+	
