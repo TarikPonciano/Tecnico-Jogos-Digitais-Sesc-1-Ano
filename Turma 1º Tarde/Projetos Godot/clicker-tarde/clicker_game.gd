ccb9ae824ce12c2a7ef0cb176e3842274a91e0d2
TarikPonciano <40394108+TarikPonciano@users.noreply.github.com>
2025-05-19 14:55:56 -0300

    Jogo Clicker Aula 2 Tarde

--- Turma 1º Tarde/Projetos Godot/clicker-tarde/clicker_game.gd
+++ Turma 1º Tarde/Projetos Godot/clicker-tarde/clicker_game.gd
@@ -2,34 +2,26 @@ extends Node2D
 
 var pontuacao = 0
 
-var poder_click = 1
+var poder_clique = 1
 
-var custo_upgrade_click = 10
-var qtd_upgrade_click = 0
+var qtd_upgrade_clique = 0
+var custo_upgrade_clique = 10
+
+func atualizarInterface():
+	$RotuloPontuacao.text = str("Pontuação: ", pontuacao)
+	$BotaoClique.text = str("Clique Aqui (+",poder_clique,")")
+	$BotaoUpgradeClique.text = str("Upgrade Clique: ", custo_upgrade_clique," pontos")
 
 func _on_botao_clique_pressed() -> void:
-	pontuacao += poder_click
+	pontuacao += poder_clique
+	atualizarInterface()
 	
-	$RotuloPontuacao.text = str("Pontuação: ", pontuacao)
-
-# Conectar o botão de upgrade ao código
-# 1. Verificar se o jogador tem pontuação maior ou igual ao custo do upgrade
-# 2. Ao conseguir comprar o upgrade deve aumentar o poder de clique do jogador
-# 3. Deve reduzir a pontuação do jogador de acordo com o custo
-# 4. Deve elevar o custo para os próximos upgrades
-# 5. Atualizar os elementos visuais com as novas informações
-#Rotulo -> pontuacao
-#BotaoClique -> poder_clique
-#BotaoUpgradeClique -> custo_upgrade_clique
 func _on_botao_upgrade_clique_pressed() -> void:
-	if pontuacao >= custo_upgrade_click:
+	if pontuacao >= custo_upgrade_clique:
+		poder_clique += 1
+		pontuacao -= custo_upgrade_clique
+		qtd_upgrade_clique += 1
+		custo_upgrade_clique *= 1.2
+		custo_upgrade_clique = int(custo_upgrade_clique)
 		
-		poder_click += 1
-		pontuacao -= custo_upgrade_click
-		custo_upgrade_click *= 1.2
-		custo_upgrade_click = int(custo_upgrade_click)
-		qtd_upgrade_click += 1
-		
-		$RotuloPontuacao.text = str("Pontuação: ", pontuacao)
-		$BotaoClique.text = str("Clique Aqui (+",poder_click,")")
-		$BotaoUpgradeClique.text = str("Clique Upgrade: ",custo_upgrade_click," pontos")
+		atualizarInterface()
