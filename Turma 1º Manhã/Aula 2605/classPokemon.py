6f36063faaaa70152a0e3da36bf87ec84e20692b
Tarik Ponciano <40394108+TarikPonciano@users.noreply.github.com>
2025-05-26 09:27:36 -0300

    Classes

---
+++ Turma 1º Manhã/Aula 2605/classPokemon.py
@@ -0,0 +1,18 @@
+import random
+class Pokemon:
+    def __init__(self, especie, tipo, hp, atk, defesa):
+        self.especie = especie
+        self.tipo = tipo
+        self.hp =int( hp * (random.randint(80,120)/100) )
+        self.atk = int( atk * (random.randint(80,120)/100) )
+        self.defesa = int( defesa * (random.randint(80,120)/100) )
+    def mostrarInformacoes(self):
+        print(f'''
+----- PokeEntry {self.especie} -----
+
+Espécie: {self.especie}
+Tipo: {self.tipo}
+HP: {self.hp}
+Atk: {self.atk}
+Defesa: {self.defesa}
+''')
---
+++ Turma 1º Manhã/Aula 2605/programaPokemon.py
@@ -0,0 +1,10 @@
+#import classPokemon
+from classPokemon import Pokemon
+
+poke1 = Pokemon("Articuno", "Gelo", 300, 50, 50)
+poke2 = Pokemon("Zapdos", "Elétrico", 250, 70, 30)
+poke3 = Pokemon("Moltres", "Fogo", 270, 60, 40)
+
+poke1.mostrarInformacoes()
+poke2.mostrarInformacoes()
+poke3.mostrarInformacoes()
