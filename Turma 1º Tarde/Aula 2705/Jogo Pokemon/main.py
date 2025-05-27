8f11e21414373af775f344644bf0aa6899975bcf
Tarik Ponciano <40394108+TarikPonciano@users.noreply.github.com>
2025-05-27 14:51:15 -0300

    Update main.py

--- Turma 1º Tarde/Aula 2705/Jogo Pokemon/main.py
+++ Turma 1º Tarde/Aula 2705/Jogo Pokemon/main.py
@@ -1,5 +1,26 @@
 from classPokemon import Pokemon
 
+def batalhaPokemonV1(pk1, pk2):
+
+    poderPk1 = pk1.hp + pk1.atk + pk1.defesa
+    poderPk2 = pk2.hp + pk2.atk + pk2.defesa
+
+    print(f"BATALHA INICIADA")
+    print()
+    print(f'''
+{pk1.especie} - {poderPk1} pontos de poder 
+                    X 
+{pk2.especie} - {poderPk2} pontos de poder''')
+    print()
+    print("-----------------------------------")
+
+    if poderPk1 > poderPk2:
+        print(f"O vencedor da batalha foi o {pk1.especie}!")
+    elif poderPk2 > poderPk1:
+        print(f"O vencedor da batalha foi o {pk2.especie}!")
+    else:
+        print(f"A BATALHA TERMINOU EM EMPATE!")
+
 poke1 = Pokemon("Charmander", "Fogo", 39, 52, 43)
 
 print(f'''
@@ -25,3 +46,5 @@ print(f'''
 ''')
 
 # Criar um sistema de batalha que usa as informações dos dois pokemons e decide o vencedor.
+
+batalhaPokemonV1(poke1, poke2)
