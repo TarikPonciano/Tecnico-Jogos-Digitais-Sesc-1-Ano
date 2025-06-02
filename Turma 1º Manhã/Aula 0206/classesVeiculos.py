a0f249b2fe7147a8dd299eec31aef19b7ea56caf
Tarik Ponciano <40394108+TarikPonciano@users.noreply.github.com>
2025-06-02 09:56:56 -0300

    Create classesVeiculos.py

---
+++ Turma 1º Manhã/Aula 0206/classesVeiculos.py
@@ -0,0 +1,9 @@
+class Veiculo:
+    def __init__(self):
+        pass
+    def acelerar(self):
+        pass
+    def desacelerar(self):
+        pass
+    def mostrarInformacoes(self):
+        pass
