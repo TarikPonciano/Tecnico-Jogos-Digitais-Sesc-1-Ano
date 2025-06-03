906a593b394fba0a0343b330ba36618b148c5e95
Tarik Ponciano <40394108+TarikPonciano@users.noreply.github.com>
2025-06-03 16:48:33 -0300

    Veículos

--- Turma 1º Tarde/Aula 0306/Atividade Veiculos/classesVeiculos.py
+++ Turma 1º Tarde/Aula 0306/Atividade Veiculos/classesVeiculos.py
@@ -1,33 +1,37 @@
 class Veiculo:
-    def __init__(self, modelo, cor, marca, tipo, velocidade):
+    def __init__(self, marca, modelo, cor, velocidade, tipo):
+        self.marca = marca
         self.modelo = modelo
         self.cor = cor
-        self.marca = marca
-        self.tipo = tipo
         self.velocidade = velocidade
+        self.tipo = tipo
+
     def acelerar(self):
         self.velocidade += 5
-        print(f"{self.modelo} {self.marca} {self.cor} acelerou e está há {self.velocidade} km/h")
-    def desacelerar(self):
+        print(f"O veículo {self.marca} {self.modelo} acelerou para {self.velocidade} km/h")
 
+    def desacelerar(self):
         self.velocidade -= 5
-        
-        if self.velocidade < 0:
-            print("Não é possível desacelerar mais. O veículo está parado!")
-            self.velocidade = 0
-        else:
-            print(f"{self.modelo} {self.marca} {self.cor} desacelerou e está há {self.velocidade} km/h")
+        print(f"O veículo {self.marca} {self.modelo} desacelerou para {self.velocidade} km/h")
+
     def parar(self):
-        self.velocidade = 0
-        print(f"{self.modelo} {self.marca} {self.cor} parou completamente!")
+        self.velocidade += 0
+        print(f"O veículo {self.marca} {self.modelo} parou completamente")
 
     def mostrarInformacoes(self):
-
         print(f'''
------ Informações do Veículo -----
-Modelo: {self.modelo}
+
+----- Informações Veículos -----
 Marca: {self.marca}
+Modelo: {self.modelo}
 Cor: {self.cor}
 Tipo: {self.tipo}
 Velocidade: {self.velocidade} km/h
 ''')
+        
+class Trem(Veiculo):
+    def __init__(self, marca, modelo, cor, velocidade):
+        super().__init__(marca, modelo, cor, velocidade, "Terrestre")
+    def buzina(self):
+        print("BEEEEEEEEEEEEM BEEEEEMMMM")
+
---
+++ Turma 1º Tarde/Aula 0306/Atividade Veiculos/main.py
@@ -0,0 +1,9 @@
+from classesVeiculos import Veiculo, Trem
+
+veic1 = Veiculo("Hyundai", "Hb20 Sedan", "Vermelhinha", 120, "Terrestre" )
+veic1.acelerar()
+veic1.desacelerar()
+
+veic2 = Trem("Ferroviaria Fortaleza", "XYZ Tchu Tchu", "Normal", 200)
+veic2.mostrarInformacoes()
+veic2.buzina()
