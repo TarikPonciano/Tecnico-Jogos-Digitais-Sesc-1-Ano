0de5f50f756f09d661af5c747cea3f0c7edb5d10
Tarik Ponciano <40394108+TarikPonciano@users.noreply.github.com>
2025-06-10 14:46:24 -0300

    Class Pokemon

---
+++ Turma 1º Tarde/Aula 1006/Projeto Pokemon/classPokemon.py
@@ -0,0 +1,30 @@
+class Pokemon:
+    def __init__(self, nroPokedex, especie, tipo, hp, atk, defesa):
+        self.nroPokedex = nroPokedex
+        self.especie = especie
+        self.tipo = tipo
+        self.hp = hp
+        self.atk = atk
+        self.defesa = defesa
+        
+    def mostrarInformacoes(self):
+        print(f'''
+------- Poke Entry -------
+              
+Número: {self.nroPokedex}
+Espécie: {self.especie}
+Tipo: {self.tipo}
+HP: {self.hp}
+ATK: {self.atk}
+DEF: {self.defesa}
+
+''')
+    def atacar(self, inimigo):
+        
+        dano = int(10 * (self.atk/inimigo.defesa))
+        inimigo.hp -= dano
+
+        print(f"{self.especie} atacou {inimigo.especie} e causou {dano} de dano!")
+        print()
+        print(f"{inimigo.especie} possui {inimigo.hp} de vida restante!")
+
